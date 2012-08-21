1ed6b465005e569640343533bb049dcd5aa3fbe3
Sean Tuck <sean.tuck11@imperial.ac.uk>
2012-08-21 18:15:10 +0100

    Revert "MODISTools_1.0"
    This reverts commit 87049b0069e0caeb24a18e2dcdde5e1a095d6df7.

--- tests/SubsetTest.r
+++
@@ -1,4 +0,0 @@
-data(SubsetExample)
-MODISSubsets(LoadDat=SubsetExample, LoadMethod='object', Product='MOD13Q1',
-    Bands=c('250m_16_days_EVI','250m_16_days_pixel_reliability'),
-    Size=c(0,0), StartDate=TRUE, DateFormat='year')
