8a09039a08018b2040785b3d9983b23fd2bf645a
seantuck12 <sean.tuck@plants.ox.ac.uk>
2014-03-26 12:36:25 +0000

    Improve data handling in MODISTimeSeries

--- R/MODISTimeSeries.R
+++ R/MODISTimeSeries.R
@@ -4,25 +4,40 @@ function(Dir, Band)
   if(!file.exists(Dir)) stop("Character string input for Dir argument does not resemble an existing file path.")
   
   file.set <- list.files(path = Dir, pattern = ".asc")
-  file.set <- file.set[grepl(pattern = Band, x = file.set)]
+  
+  file.ids <- sapply(file.set, function(x) 
+                    any(grepl(pattern = Band, x = read.csv(file = x, header = FALSE, as.is = TRUE)[ ,1]))
+              )
+  file.set <- file.set[file.ids]
+  
   if(length(file.set) < 1) stop("No downloaded files found in the requested directory for the requested data band.")
   
-  data.collector <- list(length = length(file.set))
+  data.collector <- vector(mode = "list", length = length(file.set))
+  ts.row.names <- vector(mode = "list", length = length(file.set))
+  ts.col.names <- vector(mode = "list", length = length(file.set))
   nrow.recorder <- ncol.recorder <- numeric(length = length(file.set))
   
   for(i in 1:length(file.set)){
-    data.file <- read.csv(file.set[i])
+    data.file <- read.csv(file.set[i], header = FALSE, as.is = TRUE)
+    data.file <- data.file[grepl(pattern = Band, x = data.file[ ,1]), ]
     
-    # Check the timeseries length matches those already entered into the matrix.
-    if((ncol(data.file) - 1) != nrow(res)) stop("Not all timeseries match in length")
+    data.collector[[i]] <- data.file[ ,6:ncol(data.file)]
     
-    data.collector[[i]] <- data.file[ ,2:ncol(data.file)]
-    nrow.recorder[i] <- nrow(data.file[ ,2:ncol(data.file)])
-    ncol.recorder[i] <- ncol(data.file[ ,2:ncol(data.file)])
+    nrow.recorder[i] <- nrow(as.matrix(data.file[ ,6:ncol(data.file)]))
+    ncol.recorder[i] <- ncol(as.matrix(data.file[ ,6:ncol(data.file)]))
+    
+    ts.col.names[[i]] <- paste(unique(data.file[ ,4]), "_pixel", 1:ncol.recorder[i], sep = "")
+    ts.row.names[[i]] <- data.file[ ,3]
   }
   
   res <- matrix(nrow = max(nrow.recorder), ncol = sum(ncol.recorder))
-  for(j in 1:length(data.collector)) res[1:nrow(data.collector[[i]]),1:ncol(data.collector[[i]])] <- data.collector[[i]]
+  rownames(res) <- ts.row.names[[which(nrow.recorder == max(nrow.recorder))[1]]]
+  colnames(res) <- unlist(ts.col.names)
+  
+  for(j in 1:length(data.collector)){
+    res[1:nrow.recorder[j],(sum(1, ncol.recorder[1:j]) - ncol.recorder[j]):sum(ncol.recorder[1:j])] <- 
+      as.matrix(data.collector[[j]])
+  }
   
   return(res)
 }
