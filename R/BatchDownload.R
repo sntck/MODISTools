3bb79b571c5caab1fe04da47f52c0e6de89b8336
seantuck12 <sean.tuck@plants.ox.ac.uk>
2014-04-02 18:15:41 +0100

    Fix POSIX date handling

--- R/BatchDownload.R
+++ R/BatchDownload.R
@@ -148,7 +148,7 @@ function(lat.long, dates, MODIS.start, MODIS.end, Bands, Products, Size, StartDa
             
       ##### Check that there is no missing data in the download & log download status accordingly.
       if(length(subsets) != subsets.length | any(is.na(subsets))){
-        lat.long$Status[i] <- "Missing data in subset: try downloading again", 
+        lat.long$Status[i] <- "Missing data in subset: try downloading again"
         cat("Missing information for time-series ", lat.long$ID[i], ". See subset download file.\n", sep = "")
       } else {
         lat.long$Status[i] <- "Successful download"
